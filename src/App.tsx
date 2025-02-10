50f8a5dbe3103590802b04b5bc89ca310777a039
Maicol K. Oliveira <tbh.maicol.oliveira@sympla.com.br>
2025-02-10 18:43:16 +0000

    feat: add Breadcrumb and ratio and proportion

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Postulado1 from "./pages/Postulado1_dois_pontos";
 import HomePage from "./pages/Home";
+import RuleOfThree from "./pages/razao_e_proporcao/RuleOfThree";
+import Breadcrumb from "./components/Breadcrumb";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/postulado/1" element={<Postulado1 />} />
-    </Routes>
+    <div>
+      <Breadcrumb></Breadcrumb>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/postulado/1" element={<Postulado1 />} />
+        <Route path="/ratio_and_proportion/rule_of_three" element={<RuleOfThree />} />
+      </Routes>
+    </div>
   );
 }
 
